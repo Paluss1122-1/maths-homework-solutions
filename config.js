9784f3fcfe52002009bca69fb66dde484ecb4292
Paluss1122-1 <Paluss1122@gmail.com>
2025-07-19 10:18:31 +0200

    Füge Konfigurationsdatei für Supabase und Firebase hinzu und optimiere den Import im Frontend

---
+++ config.js
@@ -0,0 +1,14 @@
+export const supabaseUrl = 'https://xmyszcahhvgcrwjyqdsq.supabase.co';
+export const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhteXN6Y2FoaHZnY3J3anlxZHNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNDM0NTksImV4cCI6MjA2NDcxOTQ1OX0.2UIRYj3R_jpS-YhhYud6d5aWfNDrPpK-jy8Vi_HP-ao';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBW0WzA9IJoEZ2GuTBMaUoWpOQildKfohU",
+  authDomain: "solutions-dashboard-7-1f6e4.firebaseapp.com",
+  projectId: "solutions-dashboard-7-1f6e4",
+  storageBucket: "solutions-dashboard-7-1f6e4.firebasestorage.app",
+  messagingSenderId: "339789398931",
+  appId: "1:339789398931:web:93c23d5dd2edd4bbc2de66",
+  measurementId: "G-LPRVVJ7RE9"
+};
+
+export const VAPID_KEY = "BKacEwWB-oQLbm-5UdughuEOJr4Ht7Cu18NxNJWKGT15YdVGdpuoJzA4Se5L2nJXLldvBE8jAKLGhzVT7o757zU"; 
